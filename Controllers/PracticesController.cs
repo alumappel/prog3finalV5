02d59cdaaae0be2b650f3de9db0072dd9c0dec22
alumappel <alumappel@gmail.com>
2023-05-23 20:27:16 +0300

    new practice working!

--- Controllers/PracticesController.cs
+++ Controllers/PracticesController.cs
@@ -140,73 +140,113 @@ namespace prog3finalV4.Controllers
         }
 
 
-        //////שמירת אימון חדש
-        ////[HttpPost("InsertPractice")]
-        //////הפונקציה מקבלת כפרמטר את השאלה
-        ////public async Task<IActionResult> InsertPractice(PracticeDTO practice)
-        ////{
-        ////    //שורה שבודקת אם הסשן חי
-        ////    int? sessionId = HttpContext.Session.GetInt32("userId");
-        ////    //לבדוק אם זה לא null ואז אפשר להמשיך לשלבים הבאים
-        ////    if (sessionId != null)
-        ////    {
-        ////        //כתיבת שאילתת ההוספה עם כל שדות השאלה פרט למזהה שמתווסף אוטומטית
-        ////        string query = "INSERT INTO practices (practice_name, date, overall_length) VALUES (@practice_name, @date, @overall_length);";
-        ////        //קריאה לקובץ DbRepository, שליחת השאילתה והשאלה והחזרת המזהה של השאלה החדשה שנוצרה
-        ////        int newpracticeId = await _db.InsertReturnId(query, practice);
-        ////        //במידה והמזהה גדול מ-0, כלומר, ההוספה הצליחה
-        ////        if (newpracticeId > 0)
-        ////        {
-
-        ////            //בדיקה שנעשתה מדידה
-        ////            //עדכון ID בכל אחת מהתת טבלאות 
-        ////            //שמירה בטבלה המתאימה
-        ////            //בדיקת שגיאה
-        ////            if (practice.movmentData.Count > 0)
-        ////            {
-        ////                foreach (movmentDataDTO m in practice.movmentData)
-        ////                {
-        ////                    m.practiceId = newpracticeId;
-        ////                }
-        ////                string Lquery = "INSERT INTO location_in_frame (frameStateOK,eyesStateOK,rightHandState,leftHandState,practiceId) VALUES (@frameStateOK,@eyesStateOK,@rightHandState,@leftHandState,@practiceId)";
-        ////                bool isSaved = await _db.SaveDataAsync(Lquery, practice.movmentData);
-        ////                if (isSaved != true)
-        ////                {
-        ////                    return BadRequest("save location failed");
-        ////                }
-        ////            }
-        ////            if (practice.pitch.measurement_time > 0)
-        ////            {
-        ////                practice.pitch.practices_Id = newpracticeId;
-        ////                string piquery = "INSERT INTO pitch (measurement_time,good_performance_time_percent,practices_Id) VALUES (@measurement_time,@good_performance_time_percent,@practices_Id)";
-        ////                bool isSaved = await _db.SaveDataAsync(piquery, practice.pitch);
-        ////                if (isSaved != true)
-        ////                {
-        ////                    return BadRequest("save pitch failed");
-        ////                }
-        ////            }
-        ////            if (practice.volume.measurement_time > 0)
-        ////            {
-        ////                practice.volume.practices_Id = newpracticeId;
-        ////                string vquery = "INSERT INTO volume (measurement_time,volume_avg,good_performance_time_percent,too_loud_performance_time_percent,too_quiet_performance_time_percent,practices_Id) VALUES (@measurement_time,@volume_avg,@good_performance_time_percent,@too_loud_performance_time_percent,@too_quiet_performance_time_percent,@practices_Id)";
-        ////                bool isSaved = await _db.SaveDataAsync(vquery, practice.volume);
-        ////                if (isSaved != true)
-        ////                {
-        ////                    return BadRequest("save volume failed");
-        ////                }
-        ////            }
-        ////            return Ok("The practice: " + practice.practice_name + " was added successfully");
 
 
+        ////שמירת אימון חדש
+        //[HttpPost("InsertPractice")]
+        ////הפונקציה מקבלת כפרמטר את השאלה
+        //public async Task<IActionResult> InsertPractice(PracticeDTO practice)
+        //{
+        //    //שורה שבודקת אם הסשן חי
+        //    int? sessionId = HttpContext.Session.GetInt32("userId");
+        //    //לבדוק אם זה לא null ואז אפשר להמשיך לשלבים הבאים
+        //    if (sessionId != null)
+        //    {
+        //        practice.userId = (int)sessionId;
+        //        //כתיבת שאילתת ההוספה עם כל שדות השאלה פרט למזהה שמתווסף אוטומטית
+        //        string query = "INSERT INTO practices (practice_name, date, overall_length,userId) VALUES (@practice_name, @date, @overall_length,@userId);";
+        //        //קריאה לקובץ DbRepository, שליחת השאילתה והשאלה והחזרת המזהה של השאלה החדשה שנוצרה
+        //        int newpracticeId = await _db.InsertReturnId(query, practice);
+        //        //במידה והמזהה גדול מ-0, כלומר, ההוספה הצליחה
+        //        if (newpracticeId > 0)
+        //        {
 
-        ////        }
-        ////        return BadRequest("The question was not saved");
-        ////    }
-        ////    else
-        ////    {
-        ////        return BadRequest("no open session");
-        ////    }
-        ////}
+        //            //בדיקה שנעשתה מדידה
+        //            //עדכון ID בכל אחת מהתת טבלאות 
+        //            //שמירה בטבלה המתאימה
+        //            //בדיקת שגיאה
+        //            if (practice.movmentData.Count > 0)
+        //            {
+        //                // Prepare the bulk insert query
+        //                string Mquery = "INSERT INTO movment_data (frameStateOK,eyesStateOK,rightHandState,leftHandState,practiceId) VALUES ";
+
+        //                // Prepare the parameter values list
+        //                List<object> parameterValues = new List<object>();
+
+        //                // Generate the parameter placeholders and collect the parameter values
+        //                string parameterPlaceholders = string.Join(",", Enumerable.Range(0, practice.movmentData.Count)
+        //                    .Select(index =>
+        //                    {
+        //                        movmentDataDTO m = practice.movmentData[index];
+        //                        m.practiceId = newpracticeId;
+        //                        parameterValues.AddRange(new object[]
+        //                        {
+        //        m.frameStateOK,
+        //    m.eyesStateOK,
+        //    m.rightHandState,
+        //    m.leftHandState,
+        //    m.practiceId
+        //                        });
+        //                        return $"(@frameStateOK{index}, @eyesStateOK{index}, @rightHandState{index}, @leftHandState{index}, @practiceId{index})";
+        //                    }));
+
+        //                // Combine the query and parameter placeholders
+        //                Mquery += parameterPlaceholders;
+
+        //                // Perform the bulk insert
+        //                bool isSaved = await _db.SaveDataAsync(Mquery, parameterValues.ToArray());
+        //                if (!isSaved)
+        //                {
+        //                    return BadRequest("Save location failed");
+        //                }
+        //            }
+        //            if (practice.audioData.Count > 0)
+        //            {
+        //                // Prepare the bulk insert query
+        //                string Aquery = "INSERT INTO audio_data (averageVolumeForMeter,pichMax,pichMin,practiceId) VALUES ";
+
+        //                // Prepare the parameter values list
+        //                List<object> AparameterValues = new List<object>();
+
+        //                // Generate the parameter placeholders and collect the parameter values
+        //                string AparameterPlaceholders = string.Join(",", Enumerable.Range(0, practice.audioData.Count)
+        //                    .Select(index =>
+        //                    {
+        //                        audioDataDTO a = practice.audioData[index];
+        //                        a.practiceId = newpracticeId;
+        //                        AparameterValues.AddRange(new object[]
+        //                        {
+        //        a.averageVolumeForMeter,
+        //    a.pichMax,
+        //    a.pichMin,
+        //    a.practiceId
+        //                        });
+        //                        return $"(@averageVolumeForMeter{index}, @pichMax{index}, @pichMin{index}, @practiceId{index})";
+        //                    }));
+
+        //                // Combine the query and parameter placeholders
+        //                Aquery += AparameterPlaceholders;
+
+        //                // Perform the bulk insert
+        //                bool isSaved = await _db.SaveDataAsync(Aquery, AparameterValues.ToArray());
+        //                if (!isSaved)
+        //                {
+        //                    return BadRequest("Save location failed");
+        //                }
+        //            }
+
+        //            return Ok("The practice: " + practice.practice_name + " was added successfully");
+
+
+
+        //        }
+        //        return BadRequest("The question was not saved");
+        //    }
+        //    else
+        //    {
+        //        return BadRequest("no open session");
+        //    }
+        //}
 
         //שמירת אימון חדש
         [HttpPost("InsertPractice")]
@@ -220,7 +260,7 @@ namespace prog3finalV4.Controllers
             {
                 practice.userId = (int)sessionId;
                 //כתיבת שאילתת ההוספה עם כל שדות השאלה פרט למזהה שמתווסף אוטומטית
-                string query = "INSERT INTO practices (practice_name, date, overall_length,userId) VALUES (@practice_name, @date, @overall_length,@userId);";
+                string query = "INSERT INTO practices (practice_name, date, overall_length,userId) VALUES (@practice_name, @date, @overall_length,@userId)";
                 //קריאה לקובץ DbRepository, שליחת השאילתה והשאלה והחזרת המזהה של השאלה החדשה שנוצרה
                 int newpracticeId = await _db.InsertReturnId(query, practice);
                 //במידה והמזהה גדול מ-0, כלומר, ההוספה הצליחה
@@ -233,74 +273,34 @@ namespace prog3finalV4.Controllers
                     //בדיקת שגיאה
                     if (practice.movmentData.Count > 0)
                     {
-                        // Prepare the bulk insert query
-                        string Mquery = "INSERT INTO movment_data (frameStateOK,eyesStateOK,rightHandState,leftHandState,practiceId) VALUES ";
-
-                        // Prepare the parameter values list
-                        List<object> parameterValues = new List<object>();
-
-                        // Generate the parameter placeholders and collect the parameter values
-                        string parameterPlaceholders = string.Join(",", Enumerable.Range(0, practice.movmentData.Count)
-                            .Select(index =>
-                            {
-                                movmentDataDTO m = practice.movmentData[index];
-                                m.practiceId = newpracticeId;
-                                parameterValues.AddRange(new object[]
-                                {
-                m.frameStateOK,
-            m.eyesStateOK,
-            m.rightHandState,
-            m.leftHandState,
-            m.practiceId
-                                });
-                                return $"(@frameStateOK{index}, @eyesStateOK{index}, @rightHandState{index}, @leftHandState{index}, @practiceId{index})";
-                            }));
-
-                        // Combine the query and parameter placeholders
-                        Mquery += parameterPlaceholders;
-
-                        // Perform the bulk insert
-                        bool isSaved = await _db.SaveDataAsync(Mquery, parameterValues.ToArray());
-                        if (!isSaved)
+                        foreach (movmentDataDTO m in practice.movmentData)
                         {
-                            return BadRequest("Save location failed");
+                            m.practiceId = newpracticeId;
+
+                            string Mquery = "INSERT INTO movment_data (frameStateOK,eyesStateOK,rightHandState,leftHandState,practiceId) VALUES (@frameStateOK,@eyesStateOK,@rightHandState,@leftHandState,@practiceId)";
+                            bool isSaved = await _db.SaveDataAsync(Mquery, m);
+                            if (isSaved != true)
+                            {
+                                return BadRequest("save movment failed");
+                            }
                         }
                     }
                     if (practice.audioData.Count > 0)
                     {
-                        // Prepare the bulk insert query
-                        string Aquery = "INSERT INTO audio_data (averageVolumeForMeter,pichMax,pichMin,practiceId) VALUES ";
-
-                        // Prepare the parameter values list
-                        List<object> AparameterValues = new List<object>();
-
-                        // Generate the parameter placeholders and collect the parameter values
-                        string AparameterPlaceholders = string.Join(",", Enumerable.Range(0, practice.audioData.Count)
-                            .Select(index =>
-                            {
-                                audioDataDTO a = practice.audioData[index];
-                                a.practiceId = newpracticeId;
-                                AparameterValues.AddRange(new object[]
-                                {
-                a.averageVolumeForMeter,
-            a.pichMax,
-            a.pichMin,
-            a.practiceId
-                                });
-                                return $"(@averageVolumeForMeter{index}, @pichMax{index}, @pichMin{index}, @practiceId{index})";
-                            }));
-
-                        // Combine the query and parameter placeholders
-                        Aquery += AparameterPlaceholders;
-
-                        // Perform the bulk insert
-                        bool isSaved = await _db.SaveDataAsync(Aquery, AparameterValues.ToArray());
-                        if (!isSaved)
+                        foreach (audioDataDTO a in practice.audioData)
                         {
-                            return BadRequest("Save location failed");
+                            a.practiceId = newpracticeId;
+
+                            string Aquery = "INSERT INTO audio_data (averageVolumeForMeter,pichMax,pichMin,practiceId) VALUES (@averageVolumeForMeter,@pichMax,@pichMin,@practiceId)";
+                            bool isSaved = await _db.SaveDataAsync(Aquery, a);
+                            if (isSaved != true)
+                            {
+                                return BadRequest("save audio failed");
+                            }
                         }
+
                     }
-                    
+
                     return Ok("The practice: " + practice.practice_name + " was added successfully");
 
 
@@ -313,8 +313,6 @@ namespace prog3finalV4.Controllers
                 return BadRequest("no open session");
             }
         }
-
-
 
 
 
