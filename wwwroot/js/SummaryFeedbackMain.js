de4583d99457043fcf8257a6ab499c9123943eb8
alumappel <alumappel@gmail.com>
2023-08-03 13:50:19 +0300

    grafs in final feedback working

--- wwwroot/js/SummaryFeedbackMain.js
+++ wwwroot/js/SummaryFeedbackMain.js
@@ -149,7 +149,286 @@ function insertDataToHtmlSummry(Data1) {
     document.getElementById("moveScore").innerHTML = moveOverAll;
     document.getElementById("totalScore").innerHTML = finalScore;
 
+    // קריאה ליצירת גרפים
+    creatCharts(Data1);
+
 }
+
+// פונקצייה ליצירת גרפיים
+function creatCharts(Data1){
+// אודיו
+// ווליום
+    let audioGoodcount = 0;
+    let audioHighCount = 0;
+    let audioLowCount = 0;
+
+    // נתונים מכיול
+    const avgVol = 0.070163540135;
+    const stvVol = 0.031185743127;
+    const lowBorderGood = 10000 * (avgVol - stvVol);
+    const highBorderGood = 10000 * (avgVol + stvVol); 
+
+    //חישוב אחוז תקין
+    Data1.audioData.forEach(obj => {
+        let currentVol = 10000 * (obj.averageVolumeForMeter);                   
+        if (currentVol < lowBorderGood){
+            audioLowCount++;
+        } 
+        else if(currentVol > highBorderGood){
+            audioHighCount++;
+        }   
+        else{
+            audioGoodcount++;
+        }  
+    });
+
+    var optionsVol = {
+        series: [audioLowCount, audioHighCount, audioGoodcount],
+        labels: ['חלש מדי', 'חזק מדי', 'תקין'],
+        colors: ['#AE9CE6', '#F0CB7F', '#9CF097'],
+        chart: {
+        type: 'donut',},
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+              return Math.round(val) + "%"
+            },
+            distributed: true,
+            style: {
+                colors: ['#000000'],
+                fontSize:['110%'], 
+                fontFamily:['Rubik, sans-serif'],
+              },
+         },
+
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+      };
+
+      var chart = new ApexCharts(document.querySelector("#responsive-chart-vol"), optionsVol);
+      chart.render();
+
+    //   ידיים
+    // ימין
+    let rightOKcount = 0;
+    let righthidedcount = 0;
+    let righttoMuchcount = 0;
+    let rightstaticcount = 0;
+    Data1.movmentData.forEach(obj => {        
+        if (obj.rightHandState == "ok") {
+            rightOKcount++;
+        }
+        else if (obj.rightHandState == "hided"){
+            righthidedcount++;
+        }
+        else if (obj.rightHandState == "toMuch"){
+            righttoMuchcount++;
+        }
+        else if (obj.rightHandState == "static"){
+            rightstaticcount++;
+        }                    
+    });
+
+    var optionsRight = {
+        series: [rightOKcount, righthidedcount, righttoMuchcount,rightstaticcount],
+        labels: ['תקין', 'מוסתר', 'תנועה רבה מדי','חוסר בתנועה'],
+        colors: ['#9CF097','#AE9CE6', '#F0CB7F', '#F073BC'],
+        chart: {
+        type: 'donut',},
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+              return Math.round(val) + "%"
+            },
+            distributed: true,
+            style: {
+                colors: ['#000000'],
+                fontSize:['110%'], 
+                fontFamily:['Rubik, sans-serif'],
+              },
+         },
+
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+      };
+
+      var chart = new ApexCharts(document.querySelector("#responsive-chart-Right"), optionsRight);
+      chart.render();
+
+
+    //   שמאל
+    let leftOKcount = 0;
+    let lefthidedcount = 0;
+    let lefttoMuchcount = 0;
+    let leftstaticcount = 0;
+    Data1.movmentData.forEach(obj => {        
+        if (obj.leftHandState == "ok") {
+            leftOKcount++;
+        }
+        else if (obj.leftHandState == "hided"){
+            lefthidedcount++;
+        }
+        else if (obj.leftHandState == "toMuch"){
+            lefttoMuchcount++;
+        }
+        else if (obj.leftHandState == "static"){
+            leftstaticcount++;
+        }                    
+    });
+
+    var optionsLeft = {
+        series: [leftOKcount, lefthidedcount, lefttoMuchcount,leftstaticcount],
+        labels: ['תקין', 'מוסתר', 'תנועה רבה מדי','חוסר בתנועה'],
+        colors: ['#9CF097','#AE9CE6', '#F0CB7F', '#F073BC'],
+        chart: {
+        type: 'donut',},
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+              return Math.round(val) + "%"
+            },
+            distributed: true,
+            style: {
+                colors: ['#000000'],
+                fontSize:['110%'], 
+                fontFamily:['Rubik, sans-serif'],
+              },
+         },
+
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+      };
+
+      var chart = new ApexCharts(document.querySelector("#responsive-chart-Left"), optionsLeft);
+      chart.render();
+
+
+    //   מיקום בפריים
+    let frameOKcount = 0;
+    let frameBadcount = 0;
+    Data1.movmentData.forEach(obj => {        
+        if (obj.frameStateOK == true) {
+            frameOKcount++;
+        }
+        else if (obj.frameStateOK == false){
+            frameBadcount++;
+        }
+
+    });
+
+    var optionsFrame = {
+        series: [frameOKcount, frameBadcount],
+        labels: ['תקין', 'לא תקין'],
+        colors: ['#9CF097', '#F073BC'],
+        chart: {
+        type: 'donut',},
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+              return Math.round(val) + "%"
+            },
+            distributed: true,
+            style: {
+                colors: ['#000000'],
+                fontSize:['110%'], 
+                fontFamily:['Rubik, sans-serif'],
+              },
+         },
+
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+      };
+
+      var chart = new ApexCharts(document.querySelector("#responsive-chart-Frame"), optionsFrame);
+      chart.render();
+
+    //   מבט לקהל
+    let eyeOKcount = 0;
+    let eyeBadcount = 0;
+    Data1.movmentData.forEach(obj => {        
+        if (obj.eyesStateOK == true) {
+            eyeOKcount++;
+        }
+        else if (obj.eyesStateOK == false){
+            eyeBadcount++;
+        }
+
+    });
+
+    var optionsEye = {
+        series: [eyeOKcount, eyeBadcount],
+        labels: ['תקין', 'לא תקין'],
+        colors: ['#9CF097', '#F073BC'],
+        chart: {
+        type: 'donut',},
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+              return Math.round(val) + "%"
+            },
+            distributed: true,
+            style: {
+                colors: ['#000000'],
+                fontSize:['110%'], 
+                fontFamily:['Rubik, sans-serif'],
+              },
+         },
+
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+      };
+
+      var chart = new ApexCharts(document.querySelector("#responsive-chart-Eyes"), optionsEye);
+      chart.render();
+
+}
+
+
 
 
 
